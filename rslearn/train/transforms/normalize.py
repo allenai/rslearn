9ddda666ca0e318e89029b5771357c18826293cb
Favyen Bastani <favyenb@allenai.org>
2024-06-18 12:55:51 -0700

    Normalize: support clipping after normalization

--- rslearn/train/transforms/normalize.py
+++ rslearn/train/transforms/normalize.py
@@ -1,6 +1,6 @@
 """Normalization transforms."""
 
-from typing import Union
+from typing import Optional, Union
 
 import torch
 
@@ -12,6 +12,7 @@ class Normalize(torch.nn.Module):
         self,
         mean: Union[float, list[float]],
         std: Union[float, list[float]],
+        valid_range: Optional[Union[tuple[float, float], tuple[list[float], list[float]]]] = None,
         input_keys: list[str] = ["image"],
         target_keys: list[str] = [],
     ):
@@ -22,12 +23,21 @@ class Normalize(torch.nn.Module):
         Args:
             mean: a single value or one mean per channel
             std: a single value or one std per channel
+            valid_range: optionally clip to a minimum and maximum value
             input_keys: which inputs to operate on (default "image")
             target_keys: which targets to operate on (default none)
         """
         super().__init__()
         self.mean = torch.tensor(mean)
         self.std = torch.tensor(std)
+
+        if valid_range:
+            self.valid_min = torch.tensor(valid_range[0])
+            self.valid_max = torch.tensor(valid_range[1])
+        else:
+            self.valid_min = None
+            self.valid_max = None
+
         self.input_keys = input_keys
         self.target_keys = target_keys
 
@@ -47,5 +57,8 @@ class Normalize(torch.nn.Module):
         ]:
             for k in key_list:
                 d[k] = (d[k] - self.mean) / self.std
+
+                if self.valid_min is not None:
+                    d[k] = torch.clamp(d[k], min=self.valid_min, max=self.valid_max)
 
         return input_dict, target_dict
