40a9987a9597205f376cb90b6de1a66dff8afce4
yawenzzzz <yawenz@allenai.org>
2024-09-17 16:13:32 +0000

    changes with pre-commit

--- rslearn/models/sam2_enc.py
+++ rslearn/models/sam2_enc.py
@@ -1,53 +1,63 @@
+"""SegmentAnything2 encoders."""
+
+from typing import Any
+
+import gcsfs
 import torch
 import torch.nn as nn
 from sam2.build_sam import build_sam2
-from typing import Any, List, Dict, Optional
 from upath import UPath
-import gcsfs
 
 
 class SAM2Encoder(nn.Module):
     """SAM2's image encoder."""
 
     def __init__(self, model_identifier: str) -> None:
-        """
-        Initializes the SAM2Encoder with a specific model configuration and checkpoint.
+        """Initializes the SAM2Encoder with a specific model configuration and checkpoint.
 
         Args:
             model_identifier: Identifier for model type.
         """
-        super(SAM2Encoder, self).__init__()
-        
-        if 'tiny' in model_identifier:
+        super().__init__()
+
+        if "tiny" in model_identifier:
             model_cfg = "sam2_hiera_t.yaml"
-            checkpoint_path = UPath("gcs://rslearn-eai/artifacts/sam2/sam2_hiera_tiny.pt")
+            checkpoint_path = UPath(
+                "gcs://rslearn-eai/artifacts/sam2/sam2_hiera_tiny.pt"
+            )
             self.backbone_channels = [
                 [4, 96],
                 [8, 192],
                 [16, 384],
                 [32, 768],
             ]
-        elif 'small' in model_identifier:
+        elif "small" in model_identifier:
             model_cfg = "sam2_hiera_s.yaml"
-            checkpoint_path = UPath("gcs://rslearn-eai/artifacts/sam2/sam2_hiera_small.pt")
+            checkpoint_path = UPath(
+                "gcs://rslearn-eai/artifacts/sam2/sam2_hiera_small.pt"
+            )
             self.backbone_channels = [
                 [4, 96],
                 [8, 192],
                 [16, 384],
                 [32, 768],
             ]
-        elif 'base' in model_identifier:
+        elif "base" in model_identifier:
             model_cfg = "sam2_hiera_b+.yaml"
-            checkpoint_path = UPath("gcs://rslearn-eai/artifacts/sam2/sam2_hiera_base_plus.pt")
+            checkpoint_path = UPath(
+                "gcs://rslearn-eai/artifacts/sam2/sam2_hiera_base_plus.pt"
+            )
             self.backbone_channels = [
                 [4, 112],
                 [8, 224],
                 [16, 448],
                 [32, 896],
             ]
-        elif 'large' in model_identifier:
+        elif "large" in model_identifier:
             model_cfg = "sam2_hiera_l.yaml"
-            checkpoint_path = UPath("gcs://rslearn-eai/artifacts/sam2/sam2_hiera_large.pt")
+            checkpoint_path = UPath(
+                "gcs://rslearn-eai/artifacts/sam2/sam2_hiera_large.pt"
+            )
             self.backbone_channels = [
                 [4, 144],
                 [8, 288],
@@ -59,16 +69,14 @@ class SAM2Encoder(nn.Module):
 
         # Build the model and remove unnecessary components
         fs = gcsfs.GCSFileSystem()
-        with fs.open(checkpoint_path, 'rb') as f:
+        with fs.open(checkpoint_path, "rb") as f:
             self.model = build_sam2(model_cfg, f)
         self._remove_unused_modules()
 
         self.encoder = self.model.image_encoder.trunk
 
     def _remove_unused_modules(self) -> None:
-        """
-        Removes unused modules from the SAM2 model.
-        """
+        """Removes unused modules from the SAM2 model."""
         del self.model.sam_mask_decoder
         del self.model.sam_prompt_encoder
         del self.model.memory_encoder
@@ -78,9 +86,8 @@ class SAM2Encoder(nn.Module):
         del self.model.obj_ptr_proj
         del self.model.image_encoder.neck
 
-    def forward(self, inputs: List[Dict[str, Any]]) -> List[torch.Tensor]:
-        """
-        Extract multi-scale features from a batch of images.
+    def forward(self, inputs: list[dict[str, Any]]) -> list[torch.Tensor]:
+        """Extract multi-scale features from a batch of images.
 
         Args:
             inputs: List of dictionaries, each containing the input image under the key 'image'.
@@ -92,9 +99,8 @@ class SAM2Encoder(nn.Module):
         features = self.encoder(images)
         return features
 
-    def get_backbone_channels(self) -> List[List[int]]:
-        """
-        Returns the output channels of the encoder at different scales.
+    def get_backbone_channels(self) -> list[list[int]]:
+        """Returns the output channels of the encoder at different scales.
 
         Returns:
             List[List[int]]: List of downsample factors and corresponding channel counts at each scale.
