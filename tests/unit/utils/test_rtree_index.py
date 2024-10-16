c9c334d149fefa6ecc3e4246123c6d0b68a60810
hgherzog <henryh@allenai.org>
2024-10-16 13:47:38 -0700

    add local cache test

--- tests/unit/utils/test_rtree_index.py
+++ tests/unit/utils/test_rtree_index.py
@@ -1,6 +1,7 @@
 import os
 import pathlib
 import random
+import tempfile
 
 import fsspec
 from upath import UPath
@@ -38,7 +39,15 @@ def test_remote_cache(tmp_path: pathlib.Path):
     assert len(result) == 1 and result[0] == "a"
 
 
-
 def test_local_cache(tmp_path: pathlib.Path):
     """Test that we can get the cached rtree index when it's on a local filesystem."""
-    pass
+    # Build rtree with one point.
+    box = [0, 0, 1, 1]
+
+    def build_rtree1(index: RtreeIndex):
+        index.insert(box, "a")
+    with tempfile.TemporaryDirectory() as cache_dir:
+        cached_dir_upath = UPath(cache_dir)
+        index = get_cached_rtree(cached_dir_upath, tmp_path, build_rtree1)
+        result = index.query(box)
+        assert len(result) == 1 and result[0] == "a"
