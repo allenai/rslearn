b8760b4eeb9d6188ee1c8dd070db2b915f77c93d
yawenzzzz <yawenz@allenai.org>
2025-02-26 01:25:29 +0000

    fix bandnames

--- rslearn/data_sources/climate_data_store.py
+++ rslearn/data_sources/climate_data_store.py
@@ -235,9 +235,10 @@ class ERA5LandMonthlyMeans(DataSource):
                 if band in bands:
                     continue
                 bands.append(band)
-
+        # rslearn band names, remove "_"
+        band_names = [band.replace("_", " ") for band in bands]
         for item in items:
-            if tile_store.is_raster_ready(item.name, bands):
+            if tile_store.is_raster_ready(item.name, band_names):
                 continue
             # Send the request to the CDS API
             # Given that ERA5 is at a very coarse resolution, we add a buffer of half a pixel size
@@ -268,4 +269,6 @@ class ERA5LandMonthlyMeans(DataSource):
                     UPath(local_nc_fname),
                     UPath(local_tif_fname),
                 )
-                tile_store.write_raster_file(item.name, bands, UPath(local_tif_fname))
+                tile_store.write_raster_file(
+                    item.name, band_names, UPath(local_tif_fname)
+                )
