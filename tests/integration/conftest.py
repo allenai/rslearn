4d6ef0b4998b6b66369e98e2ad1fbb0e3c4b8e7f
Yawen Zhang <yawenz@allenai.org>
2025-06-20 13:31:25 -0700

    format

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,6 +1,7 @@
 import os
 
 import pytest
+from google.cloud import storage
 
 from .fixtures.datasets.local_files_dataset import local_files_dataset
 from .fixtures.geometries.seattle2020 import seattle2020, tropical_forest2024
@@ -12,19 +13,19 @@ __all__ = [
 ]
 
 
-# # maybe I don't want to explictly autouse this
-# @pytest.fixture(scope="session", autouse=True)
-# def test_bucket() -> str:
-#     os.environ.setdefault("TEST_BUCKET", "test-bucket-rslearn")
-#     test_bucket = os.environ["TEST_BUCKET"]
-#     print(f"test_bucket: {test_bucket}")
-#     storage_client = storage.Client()
-#     try:
-#         storage_client.get_bucket(test_bucket)
-#         print(f"Bucket {test_bucket} exists.")
-#     except Exception as e:
-#         raise AssertionError(f"Bucket {test_bucket} does not exist: {str(e)}")
-#     return test_bucket
+# maybe I don't want to explictly autouse this
+@pytest.fixture(scope="session", autouse=True)
+def test_bucket() -> str:
+    os.environ.setdefault("TEST_BUCKET", "test-bucket-rslearn")
+    test_bucket = os.environ["TEST_BUCKET"]
+    print(f"test_bucket: {test_bucket}")
+    storage_client = storage.Client()
+    try:
+        storage_client.get_bucket(test_bucket)
+        print(f"Bucket {test_bucket} exists.")
+    except Exception as e:
+        raise AssertionError(f"Bucket {test_bucket} does not exist: {str(e)}")
+    return test_bucket
 
 
 @pytest.fixture(scope="session", autouse=True)
