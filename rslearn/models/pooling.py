e98938df70fdfd37d8cf604de80ee4f4263cddc9
ryanp <ryanp@allenai.org>
2025-07-09 16:46:55 -0700

    Add mha + attentive pool

--- rslearn/models/pooling.py
+++ rslearn/models/pooling.py
@@ -75,3 +75,52 @@ class AttentivePool(BasePool):
         weighted = out.view(M, B, 1, 1, 1) * x  # M, B, C, H, W
         pooled = weighted.sum(dim=0)  # B, C, H, W
         return [pooled]
+
+
+class DoubleAttentivePool(BasePool):
+    """Attentive pooling over the modalities and multi-headed attention over the channels."""
+
+    def __init__(
+        self, n_channels: int, height: int, width: int, num_heads: int
+    ) -> None:
+        """Initialize a new DoubleAttentivePool.
+
+        Args:
+            n_channels: number of channels in the input features
+            height: height of the input features
+            width: width of the input features
+            num_heads: number of heads in the multi-headed attention
+        """
+        super().__init__()
+        self.pool = AttentivePool(n_channels, height, width)
+        self.mha = torch.nn.MultiheadAttention(
+            embed_dim=height * width,
+            num_heads=num_heads,
+            batch_first=True,
+        )
+
+    def forward(
+        self,
+        features: list[torch.Tensor],
+        inputs: list[dict[str, Any]],
+    ) -> list[torch.Tensor]:
+        """Attentive pool over the modalities, then MHA over the channels.
+
+        We first pool over the modalities to get a BCHW tensor.
+        Then, we compute multi-headed self attention over the channels.
+
+        Unclear whether it's better to compute MHA over channels over space.
+
+        Args:
+            features: 1-list of MBCHW tensor
+            inputs: list of input dicts (not used)
+
+        Returns:
+            1-list of BCHW tensor
+        """
+        x = self.pool(features, inputs)[0]  # B, C, H, W
+        B, C, H, W = x.shape
+        x = x.flatten(start_dim=2)  # B, C, HW
+        x, _ = self.mha(x, x, x)  # B, C, HW
+        x = x.view(B, C, H, W)  # B, C, H, W
+        return [x]
