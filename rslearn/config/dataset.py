da005c567010207c654382e8c998573b92eedaf4
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-07-09 13:08:46 -0700

    Fix :crossed_fingers:

--- rslearn/config/dataset.py
+++ rslearn/config/dataset.py
@@ -342,13 +342,23 @@ class DataSourceConfig:
     def serialize(self) -> dict[str, Any]:
         """Serialize this DataSourceConfig to a config dict."""
         config_dict = self.config_dict.copy()
+
+        def serialize_item_specs(item_spec: dict) -> dict:
+            if "fnames" not in item_spec:
+                return item_spec
+            else:
+                return_dict = {
+                    k: item_spec[k] for k in item_spec.keys() if k != "fnames"
+                }
+                # we are assuming fnames is a list of Paths
+                return_dict["fnames"] = [str(p) for p in item_spec["fnames"]]
+            return return_dict
+
         if "item_specs" in config_dict:
-            print("yes item specs is here")
-            if "fnames" in config_dict["item_specs"]:
-                print("yes fnames is here")
-                config_dict["item_specs"]["fnames"] = [
-                    str(p) for p in config_dict["item_specs"]["fnames"]
-                ]
+            config_dict["item_specs"] = [
+                serialize_item_specs(item_spec)
+                for item_spec in config_dict["item_specs"]
+            ]
         return config_dict
 
     @staticmethod
@@ -413,7 +423,6 @@ class LayerConfig:
 
     def __hash__(self) -> int:
         """Return a hash of this LayerConfig."""
-        print(self.serialize())
         return hash(json.dumps(self.serialize(), sort_keys=True))
 
     def __eq__(self, other: Any) -> bool:
