d6066f6228e2ac9098dd9ab9f0c918af11512287
Patrick Johnson <84832810+APatrickJ@users.noreply.github.com>
2025-08-20 16:41:10 -0700

    Fix formatting

--- tests/integration/data_sources/test_earthdaily.py
+++ tests/integration/data_sources/test_earthdaily.py
@@ -21,14 +21,19 @@ def edc_preview_geometry() -> STGeometry:
         WGS84_PROJECTION,
         # These are the published spatial and temporal extents of the edc-preview collection.
         shapely.box(-105.14643, 39.53704, -104.76335, 39.95877),
-        (datetime(2022, 8, 9, 18, 3, 33, tzinfo=UTC), datetime(2022, 8, 15, 18, 2, 42, tzinfo=UTC)),
+        (
+            datetime(2022, 8, 9, 18, 3, 33, tzinfo=UTC),
+            datetime(2022, 8, 15, 18, 2, 42, tzinfo=UTC),
+        ),
     )
 
 
 def test_edc_preview(tmp_path: pathlib.Path, edc_preview_geometry: STGeometry) -> None:
     """Test ingesting an item corresponding to test_geometry to local filesystem."""
     band_name = "NIR"
-    data_source = EarthDaily(collection_name="edc-preview", asset_bands={"image_file_NIR": [band_name]})
+    data_source = EarthDaily(
+        collection_name="edc-preview", asset_bands={"image_file_NIR": [band_name]}
+    )
 
     query_config = QueryConfig(space_mode=SpaceMode.INTERSECTS)
     item_groups = data_source.get_items([edc_preview_geometry], query_config)[0]
@@ -40,7 +45,9 @@ def test_edc_preview(tmp_path: pathlib.Path, edc_preview_geometry: STGeometry) -
 
     layer_name = "layer"
     data_source.ingest(
-        TileStoreWithLayer(tile_store, layer_name), item_groups[0], [[edc_preview_geometry]]
+        TileStoreWithLayer(tile_store, layer_name),
+        item_groups[0],
+        [[edc_preview_geometry]],
     )
     assert tile_store.is_raster_ready(layer_name, item.name, [band_name])
 
@@ -50,7 +57,11 @@ def test_cache_dir(tmp_path: pathlib.Path, edc_preview_geometry: STGeometry) -> 
     # Use a subdirectory so we also ensure the directory is automatically created.
     cache_dir = UPath(tmp_path / "cache_dir")
     band_name = "NIR"
-    data_source = EarthDaily(collection_name="edc-preview", asset_bands={"image_file_NIR": [band_name]}, cache_dir=cache_dir)
+    data_source = EarthDaily(
+        collection_name="edc-preview",
+        asset_bands={"image_file_NIR": [band_name]},
+        cache_dir=cache_dir,
+    )
     query_config = QueryConfig(space_mode=SpaceMode.INTERSECTS)
     data_source.get_items([edc_preview_geometry], query_config)[0]
     assert len(list(cache_dir.iterdir())) > 0
