64c4bdc769ed22f14fbe0e1ae370f7e690ecacf0
Yawen Zhang <yawenz@allenai.org>
2025-11-19 16:22:44 -0800

    resize to match shape

--- tests/unit/models/test_concatenate_features.py
+++ tests/unit/models/test_concatenate_features.py
@@ -7,7 +7,7 @@ from rslearn.models.concatenate_features import ConcatenateFeatures
 
 def test_concatenate_features() -> None:
     """Test concatenating a feature map with additional features."""
-    features = [torch.randn(2, 768, 32, 32)]
+    features = [torch.randn(2, 768, 8, 8)]
     inputs = [
         {"additional_features": torch.randn(2, 32, 32)},
         {"additional_features": torch.randn(2, 32, 32)},
@@ -15,4 +15,4 @@ def test_concatenate_features() -> None:
     concatenate_features = ConcatenateFeatures(key="additional_features")
     result = concatenate_features(features, inputs)
     assert len(result) == 1
-    assert result[0].shape == (2, 768 + 2, 32, 32)
+    assert result[0].shape == (2, 768 + 2, 8, 8)
