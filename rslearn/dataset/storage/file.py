a888dc931043ab572537dccf2b483d43bd7d3e08
Favyen Bastani <favyenb@allenai.org>
2025-12-04 15:51:09 -0800

    Use @override and remove docstring.

--- rslearn/dataset/storage/file.py
+++ rslearn/dataset/storage/file.py
@@ -2,6 +2,7 @@
 
 import json
 import multiprocessing
+from typing import override
 
 import tqdm
 from upath import UPath
@@ -49,10 +50,11 @@ class FileDatasetStorage(DatasetStorage):
         """
         self.path = path
 
+    @override
     def get_window_root(self, group: str, name: str) -> UPath:
-        """Get the path where the window should be stored."""
         return Window.get_window_root(self.path, group, name)
 
+    @override
     def get_windows(
         self,
         groups: list[str] | None = None,
@@ -60,14 +62,6 @@ class FileDatasetStorage(DatasetStorage):
         show_progress: bool = False,
         workers: int = 0,
     ) -> list["Window"]:
-        """Load the windows in the dataset.
-
-        Args:
-            groups: an optional list of groups to filter loading
-            names: an optional list of window names to filter loading
-            show_progress: whether to show tqdm progress bar
-            workers: number of parallel workers, default 0 (use main thread only to load windows)
-        """
         # Avoid directory does not exist errors later.
         if not (self.path / "windows").exists():
             return []
@@ -118,14 +112,8 @@ class FileDatasetStorage(DatasetStorage):
 
         return windows
 
+    @override
     def create_or_update_window(self, window: Window) -> None:
-        """Create or update the window.
-
-        An existing window is only updated if there is one with the same name and group.
-
-        If there is a window with the same name but a different group, the behavior is
-        undefined.
-        """
         window_path = self.get_window_root(window.group, window.name)
         window_path.mkdir(parents=True, exist_ok=True)
         metadata_path = window_path / "metadata.json"
@@ -133,17 +121,8 @@ class FileDatasetStorage(DatasetStorage):
         with open_atomic(metadata_path, "w") as f:
             json.dump(window.get_metadata(), f)
 
+    @override
     def get_layer_datas(self, group: str, name: str) -> dict[str, "WindowLayerData"]:
-        """Get the window layer datas for the specified window.
-
-        Args:
-            group: the window group.
-            name: the window name.
-
-        Returns:
-            a dict mapping from the layer name to the layer data for that layer, if one
-                was previously saved.
-        """
         window_path = self.get_window_root(group, name)
         items_fname = window_path / "items.json"
         if not items_fname.exists():
@@ -156,10 +135,10 @@ class FileDatasetStorage(DatasetStorage):
 
         return {layer_data.layer_name: layer_data for layer_data in layer_datas}
 
+    @override
     def save_layer_datas(
         self, group: str, name: str, layer_datas: dict[str, "WindowLayerData"]
     ) -> None:
-        """Set the window layer datas for the specified window."""
         window_path = self.get_window_root(group, name)
         json_data = [layer_data.serialize() for layer_data in layer_datas.values()]
         items_fname = window_path / "items.json"
@@ -167,16 +146,8 @@ class FileDatasetStorage(DatasetStorage):
         with open_atomic(items_fname, "w") as f:
             json.dump(json_data, f)
 
+    @override
     def list_completed_layers(self, group: str, name: str) -> list[tuple[str, int]]:
-        """List the layers available for this window that are completed.
-
-        Args:
-            group: the window group.
-            name: the window name.
-
-        Returns:
-            a list of (layer_name, group_idx) completed layers.
-        """
         window_path = self.get_window_root(group, name)
         layers_directory = window_path / LAYERS_DIRECTORY_NAME
         if not layers_directory.exists():
@@ -191,23 +162,10 @@ class FileDatasetStorage(DatasetStorage):
 
         return completed_layers
 
+    @override
     def is_layer_completed(
         self, group: str, name: str, layer_name: str, group_idx: int = 0
     ) -> bool:
-        """Check whether the specified layer is completed in the given window.
-
-        Completed means there is data in the layer and the data has been written
-        (materialized).
-
-        Args:
-            group: the window group.
-            name: the window name.
-            layer_name: the layer name.
-            group_idx: the index of the group within the layer.
-
-        Returns:
-            whether the layer is completed.
-        """
         window_path = self.get_window_root(group, name)
         layer_dir = get_window_layer_dir(
             window_path,
@@ -216,23 +174,10 @@ class FileDatasetStorage(DatasetStorage):
         )
         return (layer_dir / "completed").exists()
 
+    @override
     def mark_layer_completed(
         self, group: str, name: str, layer_name: str, group_idx: int = 0
     ) -> None:
-        """Mark the specified layer completed for the given window.
-
-        This must be done after the contents of the layer have been written. If a layer
-        has multiple groups, the caller should wait until the contents of all groups
-        have been written before marking them completed; this is because, when
-        materializing a window, we skip materialization if the first group
-        (group_idx=0) is marked completed.
-
-        Args:
-            group: the window group.
-            name: the window name.
-            layer_name: the layer name.
-            group_idx: the index of the group within the layer.
-        """
         window_path = self.get_window_root(group, name)
         layer_dir = get_window_layer_dir(window_path, layer_name, group_idx)
         # We assume the directory exists because the layer should be materialized before
