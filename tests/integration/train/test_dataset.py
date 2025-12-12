3a34ec79cf3be8adb2fb8b75de20a93b93680d81
Favyen Bastani <fbastani@perennate.com>
2025-12-11 20:21:17 -0500

    more fix

--- tests/integration/train/test_dataset.py
+++ tests/integration/train/test_dataset.py
@@ -17,6 +17,7 @@ from rslearn.models.singletask import SingleTaskModel
 from rslearn.train.data_module import RslearnDataModule
 from rslearn.train.dataset import DataInput, ModelDataset, SplitConfig
 from rslearn.train.lightning_module import RslearnLightningModule
+from rslearn.train.model_context import ModelContext
 from rslearn.train.optimizer import AdamW
 from rslearn.train.tasks.classification import ClassificationTask
 from rslearn.train.tasks.per_pixel_regression import (
@@ -229,13 +230,16 @@ class TestResolutionFactor:
         model.eval()
         output = (
             model(
-                [
-                    {
-                        "image": torch.ones((1, 4, 4), dtype=torch.float32),
-                    }
-                ]
-            )["outputs"]
-            .detach()
+                ModelContext(
+                    inputs=[
+                        {
+                            "image": torch.ones((1, 4, 4), dtype=torch.float32),
+                        }
+                    ],
+                    metadatas=[],
+                )
+            )
+            .outputs.detach()
             .numpy()
         )
         print(output)
