7aa382e9e8103fe5527975306e2138cc8846b96c
Favyen Bastani <favyenb@allenai.org>
2026-02-10 15:17:14 -0800

    Add working test to verify AWS COGS have harmonization pre-applied.

--- tests/online/data_sources/test_aws_sentinel2_element84.py
+++ tests/online/data_sources/test_aws_sentinel2_element84.py
@@ -1,5 +1,6 @@
 import pathlib
 
+import numpy as np
 from upath import UPath
 
 from rslearn.config import (
@@ -7,6 +8,9 @@ from rslearn.config import (
     SpaceMode,
 )
 from rslearn.data_sources.aws_sentinel2_element84 import Sentinel2
+from rslearn.data_sources.planetary_computer import (
+    Sentinel2 as PlanetaryComputerSentinel2,
+)
 from rslearn.tile_stores import DefaultTileStore, TileStoreWithLayer
 from rslearn.utils.geometry import STGeometry
 
@@ -64,3 +68,34 @@ def test_materialize_all_bands(tmp_path: pathlib.Path, seattle2020: STGeometry) 
             bounds[3] - bounds[1],
             bounds[2] - bounds[0],
         )
+
+
+def test_harmonization_preapplied(seattle2020: STGeometry) -> None:
+    """Verify AWS COGs are already harmonized.
+
+    To do so, we use a known scene present in both AWS and Planetary Computer with the
+    same processing baseline, and make sure that when we materialize it
+    """
+    # Item names for the known scene. The item name is unique to each data source since
+    # neither source uses the standard Sentinel-2 scene ID.
+    aws_item_name = "S2B_10TET_20260124_0_L2A"
+    pc_item_name = "S2B_MSIL2A_20260124T191559_R056_T10TET_20260124T211343"
+    aws_data_source = Sentinel2(assets=["red"])
+    pc_source = PlanetaryComputerSentinel2(harmonize=True, assets=["B04"])
+
+    projection = seattle2020.projection
+    bounds = (
+        int(seattle2020.shp.bounds[0]),
+        int(seattle2020.shp.bounds[1]),
+        int(seattle2020.shp.bounds[2]),
+        int(seattle2020.shp.bounds[3]),
+    )
+    aws_array = aws_data_source.read_raster(
+        "unused", aws_item_name, ["B04"], projection, bounds
+    )
+    pc_array = pc_source.read_raster(
+        "unused", pc_item_name, ["B04"], projection, bounds
+    )
+    # There could be slight difference due to rounding during resampling but it should
+    # almost be exact.
+    assert np.all(np.abs(aws_array - pc_array) <= 1)
