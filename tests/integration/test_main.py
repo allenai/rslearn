e9bdf624088286b41f98b72326b7aa22b3ba884a
Favyen Bastani <fbastani@perennate.com>
2026-02-18 16:01:13 -0500

    update tests to pass --no-ignore-errors since now it is default true

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -143,6 +143,7 @@ class TestIngestion:
             "ingest",
             "--root",
             str(prepared_dataset.path),
+            "--no-ignore-errors",
         ]
         monkeypatch.setattr(sys, "argv", mock_args)
 
@@ -430,6 +431,7 @@ class TestMaterialization:
             "materialize",
             "--root",
             str(ds_path),
+            "--no-ignore-errors",
         ]
         monkeypatch.setattr(sys, "argv", mock_args)
         with pytest.raises(json.decoder.JSONDecodeError):
